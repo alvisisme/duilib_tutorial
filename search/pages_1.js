48cb68879e51def6eef80d22feab0223c3375dbd
travis <travis@localhost>
2019-07-31 01:37:29 +0000

    Automatic doxygen build

--- search/pages_1.js
+++ search/pages_1.js
@@ -5,5 +5,6 @@ var searchData=
   ['创建子窗口',['创建子窗口',['../md_doc_child_windows.html',1,'']]],
   ['初探_20duilib_20ui_20组件结构',['初探 DuiLib UI 组件结构',['../md_doc_component_architectures.html',1,'']]],
   ['动态创建控件',['动态创建控件',['../md_doc_dynamic_control.html',1,'']]],
-  ['参考引用',['参考引用',['../md_doc_reference.html',1,'']]]
+  ['参考引用',['参考引用',['../md_doc_reference.html',1,'']]],
+  ['开发环境安装和配置',['开发环境安装和配置',['../md_doc_setup_env.html',1,'']]]
 ];
--- search/pages_2.js
+++ search/pages_2.js
@@ -1,5 +1,4 @@
 var searchData=
 [
-  ['编译_20duilib_20并创建简单窗口',['编译 DuiLib 并创建简单窗口',['../md_doc_compile.html',1,'']]],
-  ['环境配置',['环境配置',['../md_doc_setup_env.html',1,'']]]
+  ['编译_20duilib',['编译 duilib',['../md_doc_compile.html',1,'']]]
 ];
